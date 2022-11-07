df6ab95cec1e4ea31ecc22774804da38c1bb8ec7
Tiaan de Beer <tdebeer.za@gmail.com>
2022-11-07 19:32:06 +0200

    Add project files.

---
+++ PRG282_Project_The Brain Cells/LoginForm.Designer.cs
@@ -0,0 +1,51 @@
+﻿namespace PRG282_Project_The_Brain_Cells
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Name = "LoginForm";
+            this.Style = MetroSet_UI.Enums.Style.Dark;
+            this.Text = "LOG INTO YOUR ACCOUNT";
+            this.TextColor = System.Drawing.Color.White;
+            this.ThemeName = "MetroDark";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
